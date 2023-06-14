cd784406f9865e96992d66a8572f98bd4ec9b4b9
programação em ação <programacaoemacaobr@gmail.com>
2023-06-13 21:19:01 -0300

    Script implementado

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# domain-info
-Script em python para obter as informações de um domínio através de um site de whois
+## Domain-info
+
+Contém um script que dado um domínio via argumento na linha de comando, obtém as informações deste determinado domínio usando o https://www.whois.com.
+
+---
+
+### Preparando o ambiente
+
+É necessário no mínimo o python `3.8` para rodar o script, então certifique-se de que sua versão atende esse requisito.
+
+Verifique sua versão através do comando:
+
+```shell
+$ python --version
+```
+
+Com o ambiente preparado, é recomendado que se crie um novo ambiente virtual para a instalação das dependências e execução do código.
+
+Para isso, basta executar esses comandos:
+
+1. Criar um novo ambiente virtual:
+    ```shell
+    $ python -m venv venv
+    ```
+
+1. Ativar o novo ambiente virtual:
+    ```shell
+    $ source venv/bin/activate
+    ```
+
+1. Realizar o download das dependências
+    ```shell
+    $ python -m pip install -r requirements.txt
+    ```
+
+---
+
+### Execução do código
+
+Para executar o código, basta executar o script `whois.py` informando o domínio como um argumento na linha de comando, dessa forma: `python whois <domínio>`
+
+Exemplo com o domínio `google.com.br`:
+
+```shell
+$ python whois google.com.br
+```
---
+++ whois.py
@@ -0,0 +1,72 @@
+from lxml import html
+import requests
+import sys
+
+def check_minimum_python_version():
+    """
+    Verifica a versão mínima do python
+    """
+    MIN_PYTHON = (3, 8)
+    if sys.version_info < MIN_PYTHON:
+        print("Expecting at least python 3.10")
+        exit(1)
+
+def check_command_line_args():
+    """
+    Verifica os argumentos da linha de comando
+    """
+    args = sys.argv
+    expected_args = 2
+    if len(args) != expected_args:
+        print("You must pass 1 argument as a domain: python whois.py google.com")
+        exit(1)
+
+def get_domain_on_args():
+    """
+    Obtém o domínio via argumentos da linha de comando
+    """
+    domain_arg_index = 1
+    domain = sys.argv[domain_arg_index]
+    if domain == "":
+        print("You must pass 1 argument as a domain: python whois.py google.com")
+        exit(1)    
+
+def get_whois_page(domain: str) -> str:
+    """
+    Obtém a página em HTML do whois dado o domínio recebido
+    """
+    url = f'https://www.whois.com/whois/{domain}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception("received a non 200 status code")
+    html_content = html.fromstring(response.content)
+    return html_content
+
+def extract_data_from_html(html: str) -> str:
+    """
+    Extrai os dados do HTML usando XPaths
+    """
+    data_list = html.xpath('//pre[@class="df-raw"]/text()')
+    return ' '.join(data_list)
+
+def print_data(domain: str, data: str):
+    """
+    Imprime as informações formatando o texto
+    """
+    print(f"---------------------- {domain} ----------------------\n")
+    formatted_data = data.replace("\\n","\n")
+    print(formatted_data)
+
+if __name__ ==  "__main__":
+    check_minimum_python_version()
+    check_command_line_args()
+    domain = get_domain_on_args()    
+    try:
+        html_content = get_whois_page(domain)
+        data = extract_data_from_html(html_content)
+        print_data(domain, data)
+    except Exception as e:
+        print(f"Something wrong while processing whois consult to {domain}: {e}")
+        exit(1)
+        
+        
